d14b8531c50f83a1eb95a20002109414f18bf72e
Radu Lucut <lucutradu@gmail.com>
2023-01-21 00:05:47 +0200

    first commit

---
+++ 2022-01-20/cf_59A.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	str, _ := reader.ReadString('\n')
+	str = strings.TrimSpace(str)
+
+	uppercaseCount := 0
+	for i := 0; i < len(str); i++ {
+		if str[i] < 97 {
+			uppercaseCount++
+		}
+	}
+
+	doUppercase := uppercaseCount > len(str)-uppercaseCount
+
+	for i := 0; i < len(str); i++ {
+		if doUppercase && str[i] >= 97 {
+			fmt.Print(string(str[i] - 32))
+
+			continue
+		}
+
+		if !doUppercase && str[i] < 97 {
+			fmt.Print(string(str[i] + 32))
+
+			continue
+		}
+
+		fmt.Printf("%c", str[i])
+	}
+
+	fmt.Println()
+}
